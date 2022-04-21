5a8fd442cc18b8d74055a610a68a56318fa4d0ea
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-04-21 21:17:46 +0200

    add code who don't do the job

--- check_checkbox/index.js
+++ check_checkbox/index.js
@@ -1,28 +1,25 @@
-const checkboxes = document.querySelectorAll(
-  '.inbox input[type="checkbox"]'
-);
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
+
 function handleCheck(e) {
-  //we need to check if they had the shift key down
-  //and check that they are checking it not unchecking it
   let inBetween = false;
   if (e.shiftKey && this.checked) {
-    //loop over every single checkbox
-    checkboxes.forEach((checkbox) => {
-      // console.log(checkbox);
+
+    checkboxes.forEach(checkbox => {
+      console.log(checkbox);
       if (checkbox === this || checkbox === lastChecked) {
         inBetween = !inBetween;
-        // console.log("starting to check in between");
+        console.log('Starting to check them in between!');
       }
+
       if (inBetween) {
         checkbox.checked = true;
       }
     });
   }
+
   lastChecked = this;
 }
 
-checkboxes.forEach((checkbox) =>
-  checkbox.addEventListener("click", handleCheck)
-);
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
