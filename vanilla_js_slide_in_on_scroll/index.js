62e3af6a57eee0589de6c7afdcbfdd12a062c1f8
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-05-22 16:46:37 +0200

    first commit vanilla js slide in on scroll

---
+++ vanilla_js_slide_in_on_scroll/index.js
@@ -0,0 +1,14 @@
+function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function() {
+        const context = this, args = arguments;
+        const later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context,args);
+    };
+};
