2f99a7ba24553eb82315dffdc7a2039dc7de0031
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-05-22 16:35:29 +0200

    last commit konami code

--- konami_code/index.js
+++ konami_code/index.js
@@ -4,5 +4,10 @@ const secretCode = "s3cr3t";
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
     pressed.push(e.key);
+    pressed.splice(-secretCode.lenght - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        console.log('WELL DONE !');
+        cornify_add();
+    }
     console.log(pressed);
 })
