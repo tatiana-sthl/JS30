fcbb6fa5f373b499d044209d61e1dd2b8f670d01
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-05-23 10:47:46 +0200

    understanding update array

--- reference_vs_copy/index.js
+++ reference_vs_copy/index.js
@@ -4,3 +4,22 @@ let age2 = age;
 console.log(age, age2);
 age = 200;
 console.log(age, age2);
+
+let name = "Tati";
+let name2 = name;
+console.log(name, name2);
+name = 'Tatiana';
+console.log(name, name2);
+
+// We have an array
+const players = ['Tati', 'Clem', 'Lucie', 'Baptiste'];
+
+// and we want to make a copy of it :
+const team = players;
+console.log(players, team)
+
+// Maybe we can juste do somehting like this :
+team[3] = 'Lux';
+
+/* What happens when we update that array ?
+*/
