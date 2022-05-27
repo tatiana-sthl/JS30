52cde1bcfea0081247b8db4d3764311c410a07b4
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-05-27 11:35:27 +0200

    add function item

--- local_storage_and_event_delegation/index.js
+++ local_storage_and_event_delegation/index.js
@@ -1,3 +1,16 @@
 const addItems = document.querySelector('.add-plant');
 const itemsList = document.querySelector('.plants');
 const items = [];
+
+function addItem(e) {
+    e.preventDefault();
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    };
+
+    this.reset();
+}
+
+addItems.addEventListener('submit', addItem);
