0d9d1e341a024c397a6fb5e14eb1f49cab8f609c
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-06-17 15:11:32 +0200

    last commit

--- follow_along_links/index.js
+++ follow_along_links/index.js
@@ -4,7 +4,18 @@ highlight.classList.add('highlight');
 document.body.append(highlight);
 
 function highlightLink() {
-    console.log(this);
+    const linkCoords = this.getBoundingClientRect();
+
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    };
+
+    highlight.style.width = `${linkCoords.width}px`;
+    highlight.style.height = `${linkCoords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
 }
 
 triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
