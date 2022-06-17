9363fc48f529a77d9dd0dc9d617f09087628f1bb
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-06-17 14:55:40 +0200

    finally start to add js

---
+++ follow_along_links/index.js
@@ -0,0 +1,10 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLink() {
+    console.log(this);
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
