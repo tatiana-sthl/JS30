6f6bf35e4ec0bf24df0dcb006f9a156fc1b59451
Tatiana Stahli <stahli.tatiana@gmail.com>
2022-06-30 14:20:44 +0200

    add js

--- strip_follow_along_dropdown_nav/index.js
+++ strip_follow_along_dropdown_nav/index.js
@@ -3,7 +3,8 @@ const background = document.querySelector('.dropdownBackground');
 const nav = document.querySelector('.top');
 
 function handleEnter() {
-    console.log('Enter');
+    this.classList.add('trigger-enter');
+    setTimeout(() => this.classList.add('trigger-enter-active'), 150);
 }
 
 function handleLeave() {
